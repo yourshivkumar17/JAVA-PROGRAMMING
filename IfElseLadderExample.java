05e9779205b8069f134ecd68fb0ae3eac6eeabbc
Shiv Kumar <yourshivkumar17@gmail.com>
2025-08-21 10:20:48 +0530

    Add files via upload

---
+++ IfElseLadderExample.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class IfElseLadderExample {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter your marks: ");
+        int marks = sc.nextInt();
+
+        if (marks >= 90) {
+            System.out.println("Grade: A");
+        } else if (marks >= 80) {
+            System.out.println("Grade: B");
+        } else if (marks >= 70) {
+            System.out.println("Grade: C");
+        } else if (marks >= 60) {
+            System.out.println("Grade: D");
+        } else {
+            System.out.println("Grade: F");
+        }
+
+        sc.close();
+    }
+}
