375d166fe7b9fbbf215aa44c64116e8707f48e10
Shiv Kumar <yourshivkumar17@gmail.com>
2025-11-08 18:26:28 +0530

    Create ChekPalindromString.java

---
+++ ChekPalindromString.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String str = sc.nextLine();
+        String rev = "";
+        for (int i = str.length() - 1; i >= 0; i--) {
+            rev = rev + str.charAt(i);
+        }
+        if (str.equalsIgnoreCase(rev))
+            System.out.println("Palindrome");
+        else
+            System.out.println("Not Palindrome");
+    }
+}
