7bd7de2d74e8adf4fb3d8b11979ad2f6a6578072
Shiv Kumar <yourshivkumar17@gmail.com>
2025-11-30 17:31:01 +0530

    Create ReverseLinkedList.java

---
+++ ReverseLinkedList.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        LinkedList <Integer> list = new LinkedList<>();
+        int n = sc.nextInt();
+        for(int i =0; i<n;i++){
+            list.add(sc.nextInt());
+        }
+        Collections.reverse(list);
+        System.out.println("Reversed list"+ list);
+    }
+}
